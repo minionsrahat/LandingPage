f1226fea5c5e2800443e9f035fd935eb6d805116
minionsrahat <rahatuddin786@gmail.com>
2022-06-05 20:08:13 +0600

    Footer section added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-
 import './App.css';
 import About from './Components/About/About';
+import BookAppointment from './Components/BookAppointment/BookAppointment';
 import Navbar from './Components/Navbar/Navbar';
 import Service from './Components/Service/Service';
 import Slider from './Components/Slider/Slider';
 import WhyChooseUs from './Components/WhyChooseUs/WhyChooseUs';
+import Footer from './Components/Footer/Footer' ;
 
 function App() {
   return (
     <>
-    <div className="bg-gray-100">
-    <Navbar></Navbar>
-      <Slider></Slider>
-      <About></About>
-      <Service></Service>
-      <WhyChooseUs></WhyChooseUs>
-    </div>
-      
+      <div className="bg-gray-100">
+        <Navbar></Navbar>
+        <Slider></Slider>
+        <About></About>
+        <Service></Service>
+        <WhyChooseUs></WhyChooseUs>
+        <BookAppointment></BookAppointment>
+        <Footer></Footer>
+      </div>
+    
+
     </>
   );
 }
---
+++ src/Components/BookAppointment/BookAppointment.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const BookAppointment = () => {
+    return (
+        <>
+            <section
+                class="relative bg-blue-teal-gradient px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-12 text-center md:text-left">
+                <div class="md:flex md:items-center md:justify-center">
+                    <h2 class="text-xl font-bold text-white">Get in touch with us today! Call us on: +1
+                        562-789-1935</h2>
+                    <a href="#"
+                        class="px-8 py-4 bg-white text-blue-600 rounded inline-block font-semibold md:ml-8 mt-4 md:mt-0">Book
+                        Appointment</a>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default BookAppointment;
