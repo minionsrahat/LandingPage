921d4ae9434e2ff3daee1b9e5200397f21dd5ff7
minionsrahat <rahatuddin786@gmail.com>
2022-06-05 19:04:58 +0600

    Slider addd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Slider from './Components/Slider/Slider';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Slider></Slider>
     </>
   );
 }
